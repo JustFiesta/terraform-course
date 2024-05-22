61372b8327a1ed96bb32e9029d2481a5b9773520
JustFiesta <mateusz.bocak9@gmail.com>
2024-05-22 12:17:40 +0200

    Fixed naming and restructured file

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -6,10 +6,10 @@ output "subnet_id" {
   value = aws_subnet.tf-subnet.id
 }
 
-output "eip_ec2_id" {
-  value = aws_eip.public_ip.id
+output "sec_group_id" {
+  value = aws_security_group.tf-sec-group.id
 }
 
-output "sec_group_id" {
-  value = aws_security_group.my_security_group.id
+output "eip_ec2_id" {
+  value = aws_eip.tf-public-ip.id
 }
