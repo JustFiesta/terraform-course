e086d404d3d0e0a4f263c7f793ffe9d3a6518e30
JustFiesta <mateusz.bocak9@gmail.com>
2024-05-22 12:38:12 +0200

    Removed dependency on network.vpc_id - it should get it from output

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,11 +1,11 @@
-# Create temporary instance with httpd running
 resource "aws_instance" "tf-tmp-instance" {
   ami           = "ami-005e54dee72cc1d00"
   instance_type = "t3.micro"
   user_data     = var.user_data
   
-  depends_on    = [module.network.vpc_id]
-  
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.sec_group_id]
+
   tags = merge(var.common_tags, { Name = "tf-tmp-instance" })
 
   lifecycle {
@@ -17,7 +17,6 @@ resource "aws_instance" "tf-tmp-instance" {
 resource "aws_ami_from_instance" "tf-ami-tmp-instance" {
   name               = "tf-ami-tmp-instance"
   source_instance_id = aws_instance.tf-tmp-instance.id
-  depends_on         = [aws_instance.tf-tmp-instance]
 
   tags = merge(var.common_tags, { Name = "tf-ami-tmp-instance" })
 }
