891854cefb15fea2fb3e4e1aac3e377124b5fc7b
Mateusz Bocak <mb125104@stud.ur.edu.pl>
2025-04-01 16:40:58 +0200

    Refactor network outputs: update references to load balancer and VPC module outputs

--- t1-course/tf-module-final-project/network/outputs.tf
+++ t1-course/tf-module-final-project/network/outputs.tf
@@ -1,29 +1,29 @@
 output "lb_dns_name" {
   description = "The DNS name of the load balancer"
-  value       = aws_lb.main.dns_name
+  value       = module.load_balancer.lb_dns_name
 }
 
 output "target_group_arn" {
   description = "value of the target group ARN"
-  value       = aws_lb_target_group.main.arn
+  value       = module.load_balancer.target_group_arn
 }
 
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.main.id
+  value       = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
   description = "The IDs of the subnets"
-  value = aws_subnet.public[*].id
+  value       = module.vpc.public_subnet_ids
 }
 
 output "private_subnet_ids" {
   description = "The IDs of the subnets"
-  value = aws_subnet.private[*].id
+  value       = module.vpc.private_subnet_ids
 }
 
 output "security_group_ids" {
   description = "The IDs of the security groups"
-  value       = aws_security_group.main.id
+  value       = module.vpc.security_group_ids
 }
