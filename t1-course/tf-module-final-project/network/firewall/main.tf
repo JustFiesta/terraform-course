b6f9a74ce3153e7d31551f362fc2ea99c54c5e22
Mateusz Bocak <99048876+JustFiesta@users.noreply.github.com>
2025-04-03 16:34:13 +0200

    Update ingress rule in security group to use cidr_blocks instead of security_groups

--- t1-course/tf-module-final-project/network/firewall/main.tf
+++ t1-course/tf-module-final-project/network/firewall/main.tf
@@ -36,10 +36,10 @@ resource "aws_security_group" "instance" {
   }
 
   ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
